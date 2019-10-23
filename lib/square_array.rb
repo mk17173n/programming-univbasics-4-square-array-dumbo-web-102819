4491e855e99f46ec5b4f440701663f554d4fe23a
Monsur Khan <monsur.khan@gmail.com>
2019-10-23 00:02:19 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,9 @@
-def square_array(element)
+def square_array(array)
   index =0
   squared_array =[]
   while index < array.size do
     squared_array.push(array[index]**2)
-    index +=10
+    index +=1
+  end
     return squared_array
 end
